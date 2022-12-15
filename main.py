cf319565225bac672c7bea3431d610440c432ba7
Tawitg <108408219+Tawitg@users.noreply.github.com>
2022-12-15 21:34:45 +0000

    More efficient code  + comments

--- main.py
+++ main.py
@@ -1,41 +1,60 @@
 from socket import *
-import time
-import re
+import timeit
 
-portRangeFormat = re.compile("([0-9]+)-([0-9]+)")
+open_ports = []
 
-runTime = time.time()
+if __name__ == "__main__":
+    # Prompt the user to enter a host for scanning
+    target = input("Enter host for scanning (Type 127.0.0.1 for localhost): ")
+    # Convert the hostname to an IP address
+    target_IP = gethostbyname(target)
 
-channel = socket(AF_INET, SOCK_STREAM)
+    # Prompt the user to enter a range of ports to scan
+    port_range = input("Enter the range of ports to scan (e.g 0-1024) or a single port (e.g 445): ")
 
-openPorts=[]   
+    # If the port range contains a dash, split it into start_port and end_port
+    if "-" in port_range:
+        start_port, end_port = map(int, port_range.split("-"))
+    # Otherwise, treat the entire input as the value for start_port
+    else:
+        start_port = int(port_range)    
+        # If a single port is provided, end_port will be the same as start_port
+        end_port = start_port
 
-if __name__=="__main__":
-    target = input('Enter host for scanning (Type "127.0.0.1" for localhost): ').lower()
-    targetIP = gethostbyname(target)
+
+    # Print the target IP address and port range being scanned
+    print(f"\nScanning port(s) {port_range} on host : {target_IP}\n")
+
+    # Record the start time of the scan
+    start_time = timeit.default_timer() 
+
+    # Scan each port in the specified range
+    for port in range(start_port, end_port + 1):
+        # Create a new socket
+        sock = socket(AF_INET, SOCK_STREAM)
+        # Set a timeout of 0.0001 seconds for the socket
+        sock.settimeout(1e-04)
+        # Try to connect to the port
+        connection = sock.connect_ex((target_IP, port))
+        # If the connection is successful, print the port number and add it to the list of open ports
+        if connection == 0:
+            print(f"Port {port}: OPEN")
+            open_ports.append(port)
+        # Close the socket
+        sock.close()
+
+    # If the list of open ports is empty, set it to None
+    if open_ports==[]:
+        open_ports=[None]
+
+    # Print the list of open ports
+    print(f"\nOpen ports : {open_ports}")
+
+    # Record the end time of the scan
+    end_time = timeit.default_timer()
     
-    while True:
-        portRange = input("Enter the range of ports you want to scan in <int>-<int> format (e.g 20-25) : ")
-        
-        portRangeValidFormat = portRangeFormat.search(portRange.replace(" ",""))
-        
-        if portRangeValidFormat:
-          minPortRange=int(portRangeValidFormat.group(1))
-          maxPortRange=int(portRangeValidFormat.group(2))
-          break
+    # Print the time taken to complete the scan
+    print(f"Scan complete. Time taken: {end_time-start_time:.2f} seconds")
 
-    print(f'\nScanning port(s): {portRange} on host: {targetIP}\n')
-    
-    for i in range(minPortRange, maxPortRange):
-        connection = channel.connect_ex((targetIP, i))
-        
-        print(f'Port {i}: OPEN') if connection==0 else print(f'Port {i}: CLOSED')
-        
-        if connection==0:
-                openPorts.append(i)
-    
-    channel.close
-
-print(f'\nOpen ports : {openPorts}')
-print(f'Time taken: {time.time() - runTime}')
-input('\nPress ENTER to exit')
+    # Wait for the user to press ENTER before closing the program
+    input('\nPress ENTER to exit')
