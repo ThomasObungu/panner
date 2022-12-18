cf9e82cea459e7f5f32943e1dcec147c4cca5452
Tawitg <108408219+Tawitg@users.noreply.github.com>
2022-12-18 18:22:14 +0000

    Update main.py
    Made code faster

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from socket import *
+import threading
 import time
 import timeit
 from os import system, name
+
 
 class PortScan:
     def __init__(self, target, start_port, end_port):
@@ -16,27 +18,38 @@ class PortScan:
 
     def get_target(self):
         try:
-            target_IP = gethostbyname(self.target)
+            self.target_IP = gethostbyname(self.target)
         except gaierror:
             print(f"Invalid host IP address: {self.target}\nPlease try again.")
             time.sleep(2)
             self.clear_screen()
             self.get_target()
 
+
+    def scan_port(self, port):
+            sock = socket(AF_INET, SOCK_STREAM)
+            sock.settimeout(1e-04)
+            self.connection = sock.connect_ex((self.target, port))
+            if self.connection == 0:
+                print(f"Port {port}: OPEN")
+                self.open_ports.append(port)
+            sock.close()
+   
     def scan_ports(self):
         print(f"\nScanning port(s) {self.start_port}-{self.end_port} on host : {self.target}\n")
         start_time = timeit.default_timer() 
 
-        for port in range(self.start_port, self.end_port + 1):
-            sock = socket(AF_INET, SOCK_STREAM)
-            sock.settimeout(1e-04)
-            connection = sock.connect_ex((self.target, port))
-            if connection == 0:
-                print(f"Port {port}: OPEN")
-                self.open_ports.append(port)
-            sock.close()
+        threads = [] 
         
-        if self.start_port == self.end_port and connection == 1 or self.open_ports == []:
+        for port in range(self.start_port, self.end_port + 1):
+            thread = threading.Thread(target=self.scan_port, args=(port,))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+        
+        if self.start_port == self.end_port and self.connection == 1 or self.open_ports == []:
             print(f'Port(s) {self.start_port}-{self.end_port}: CLOSED')
         if self.open_ports == []:
             self.open_ports = [None]
